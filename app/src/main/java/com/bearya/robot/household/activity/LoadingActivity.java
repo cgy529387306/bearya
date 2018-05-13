6551fbef7a93b2f37120719ad982946097a2976d
cgy529387306 <529387306@qq.com>
2018-05-13 16:13:43 +0800

    xxx

--- app/src/main/java/com/bearya/robot/household/activity/LoadingActivity.java
+++ app/src/main/java/com/bearya/robot/household/activity/LoadingActivity.java
@@ -2,13 +2,17 @@ package com.bearya.robot.household.activity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.WindowManager;
 
 import com.bearya.robot.household.R;
 import com.bearya.robot.household.utils.NavigationHelper;
-import com.bearya.robot.household.utils.SharedPrefUtil;
 import com.bearya.robot.household.utils.UserInfoManager;
 import com.bearya.robot.household.views.BaseActivity;
+import com.tencent.android.tpush.XGIOperateCallback;
+import com.tencent.android.tpush.XGPushConfig;
+import com.tencent.android.tpush.XGPushManager;
+import com.tencent.android.tpush.common.Constants;
 
 /**
  * Created by cgy on 2018/4/19 0019.
@@ -23,6 +27,7 @@ public class LoadingActivity extends BaseActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        initXg();
         new Handler().postDelayed(new Runnable() {
 
             public void run() {
@@ -33,5 +38,29 @@ public class LoadingActivity extends BaseActivity {
                 }
             }
         }, LOADING_TIME_OUT);
+    }
+
+
+    private void initXg(){
+        XGPushConfig.enableDebug(this, true);
+        XGPushConfig.getToken(this);
+        XGPushManager.registerPush(getApplicationContext(),
+                new XGIOperateCallback() {
+                    @Override
+                    public void onSuccess(Object data, int flag) {
+                        Log.w(Constants.LogTag, "+++ register push sucess. token:" + data + "flag" + flag);
+                    }
+
+                    @Override
+                    public void onFail(Object data, int errCode, String msg) {
+                        Log.w(Constants.LogTag,
+                                "+++ register push fail. token:" + data
+                                        + ", errCode:" + errCode + ",msg:"
+                                        + msg);
+                    }
+                });
+
+        XGPushConfig.getToken(this);
+
     }
 }
